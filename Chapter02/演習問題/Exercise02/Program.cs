f1f7cca4ceb346d33fcd9b44f48395c1b9a2d80e
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-04-23 10:19:05 +0900

    機能追加①完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -3,12 +3,38 @@ namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("***　変換アプリ　***");
+            Console.WriteLine("１：インチからメートル");
+            Console.WriteLine("２：メートルからインチ");
+            Console.Write("＞" );
+            int num = int.Parse(Console.ReadLine());
 
-            //インチからメートルへの対応表を出力
-            for (int inch = 1; inch <= 10; inch++) {
-                double meter = inch * 0.0254;
-                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            Console.Write("はじめ：");　//コンソール入力
+            int start = int.Parse(Console.ReadLine());　//文字列で取り込んで整数へ変換
+            Console.Write("おわり：");　//コンソール入力
+            int end = int.Parse(Console.ReadLine());　//文字列で取り込んで整数へ変換
+
+            if(num == 1){
+                PrintInchToMeterList(start, end);
+            } else {
+                PrintMeterToInchList(start, end);
             }
+
+                //インチからメートルへの対応表を出力
+                static void PrintInchToMeterList(int start, int end) {
+                    for (int inch = start; inch <= end; inch++) {
+                        double meter = InchConverter.ToMeter(inch);
+                        Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+                    }
+                }
+
+                //メートルからインチへの対応表を出力
+                static void PrintMeterToInchList(int start, int end) {
+                    for (int meter = start; meter <= end; meter++) {
+                        double inch = InchConverter.FromMeter(meter);
+                        Console.WriteLine($"{meter}m = {inch:0.0000}inch");
+                    }
+                }
         }
     }
 }
