e57c638734b28b030ebac4fb0a698b5a873db874
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-04-23 09:40:34 +0900

    インチからメートル変換完成

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            //インチからメートルへの対応表を出力
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = inch * 0.0254;
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            }
+        }
+    }
+}
