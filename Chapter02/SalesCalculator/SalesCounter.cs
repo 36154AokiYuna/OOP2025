d7d8eafbf4c8ab1602c0aaf149a1377b5bc0113c
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-09 15:20:07 +0900

    コンストラクタの引数変更

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -10,14 +10,14 @@ namespace SalesCalculator {
         private readonly List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
         //店舗別売り上げを求める
         public Dictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (Sale sale in _sales) {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;
                 else
