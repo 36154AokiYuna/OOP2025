42abf2e662ae6b1d2d54dacc50aef9fececd450a
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-13 11:48:07 +0900

    2.1.4完成

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -8,11 +8,11 @@ namespace Exercise01 {
     //2.1.1
     public class Song {
         //歌のタイトル
-        public string Title { get; set; } = string.Empty;
+        public string Title { get; private set; } = string.Empty;
         //アーティスト名
-        public string ArtistName { get; set; } = string.Empty;
+        public string ArtistName { get; private set; } = string.Empty;
         //演奏時間、単位は秒
-        public int Length { get; set; }
+        public int Length { get; private set; }
 
         //2.1.2 コンストラクタ
         public Song(string title, string artistname, int length) {
