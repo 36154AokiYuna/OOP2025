3e28c9bdc107965c74f6d67144a9665550dfe3d5
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-13 10:23:05 +0900

    2.1.2完成

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -13,8 +13,12 @@ namespace Exercise01 {
         public string ArtistName { get; set; } = string.Empty;
         //演奏時間、単位は秒
         public int Length { get; set; }
+
+        //2.1.2 コンストラクタ
+        public Song(string title, string artistname, int length) {
+            this.Title = title;
+            this.ArtistName = artistname;
+            this.Length = length;
+        }
     }
-
-    //2.1.2
-
 }
