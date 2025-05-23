c002d5193b7d560a454eacb90ef0c0eef8ffd153
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-23 15:20:34 +0900

    4.2.1完成

---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            if (int.TryParse(Console.ReadLine(), out var num)) {
+                if (num < 0) {
+                    Console.WriteLine(num);
+                } else if (num < 100) {
+                    Console.WriteLine(num * 2);
+                } else if (num < 500) {
+                    Console.WriteLine(num * 3);
+                } else {
+                    Console.WriteLine(num);
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }      
+        }
+    }
+}
