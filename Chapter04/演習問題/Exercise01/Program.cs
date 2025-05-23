55cd94ce7bf30522d91eba34b2cc03a7f2871ca1
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-23 13:45:54 +0900

    4.1.1完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -15,15 +15,35 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            //foreach文
+            var lang = langs.Where(l => l.Contains("S"));
+            foreach (var name in lang) {
+                Console.WriteLine(name);
+            }
+
+            //for文
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains("S")) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+            //While文
+            int k = 0;
+            while (k < langs.Count) {
+                if (langs[k].Contains("S")) {
+                    Console.WriteLine(langs[k]);
+                }
+                k++;
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-            
+
         }
 
         private static void Exercise3(List<string> langs) {
-            
+
         }
     }
 }
