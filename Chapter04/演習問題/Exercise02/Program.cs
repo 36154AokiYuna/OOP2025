44f246ae758ebd20bba9393a570b37677774a33a
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-23 15:42:17 +0900

    4.2.2と4.2.3終わってなかった

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Exercise02 {
+﻿
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
+            Exercise01();
+            Exercise02();
+            Exercise03();      
+        }
+
+        private static void Exercise01() {
             if (int.TryParse(Console.ReadLine(), out var num)) {
                 if (num < 0) {
                     Console.WriteLine(num);
@@ -14,7 +21,44 @@
                 }
             } else {
                 Console.WriteLine("入力値に誤りがあります");
-            }      
+            }
+        }
+
+        private static void Exercise02() {
+            var line = Console.ReadLine();
+            if(int.TryParse(line, out var num)) {
+                switch (num) {
+                    case < 0:
+                        Console.WriteLine(num);
+                        break;
+                    case < 100:
+                        Console.WriteLine(num * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(num * 3);
+                        break;
+                    default:
+                        Console.WriteLine(num);
+                        break;
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            } 
+        }
+
+        private static void Exercise03() {
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                var text = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num * 3,
+                    _ => num
+                };
+                Console.Write(text);
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
     }
 }
