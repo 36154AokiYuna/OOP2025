b8262c699618f73b3d8c01cdb54aa9f4aa162810
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-23 14:48:55 +0900

    4.1.3完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Xml.Linq;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
@@ -16,7 +18,7 @@ namespace Exercise01 {
 
         private static void Exercise1(List<string> langs) {
             //foreach文
-            var lang = langs.Where(l => l.Contains('S'));
+            var lang = langs.Where(s => s.Contains('S'));
             foreach (var name in lang) {
                 Console.WriteLine(name);
             }
@@ -50,14 +52,14 @@ namespace Exercise01 {
             Console.WriteLine("");  //改行
 
             //for文
-            var lang = langs.Where(l => l.Contains('S')).ToList();
+            var lang = langs.Where(s => s.Contains('S')).ToList();
             for(int i = 0; i < lang.Count; i++) {
                 Console.WriteLine(lang[i]);
             }
             Console.WriteLine("");  //改行
 
             //While文
-            var lange = langs.Where(l => l.Contains('S')).ToList();
+            var lange = langs.Where(s => s.Contains('S')).ToList();
             int k = 0;
             while (k < lange.Count) {
                     Console.WriteLine(lange[k]);
@@ -66,7 +68,9 @@ namespace Exercise01 {
         }
 
         private static void Exercise3(List<string> langs) {
-
+            //2行で完結させる
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
+            Console.WriteLine(lang);
         }
     }
 }
