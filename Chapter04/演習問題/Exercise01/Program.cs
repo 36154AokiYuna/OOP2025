856f7a9f4ff922672dec0b916dd14f20eee0bf63
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-23 14:06:53 +0900

    4.1.2完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -16,22 +16,24 @@ namespace Exercise01 {
 
         private static void Exercise1(List<string> langs) {
             //foreach文
-            var lang = langs.Where(l => l.Contains("S"));
+            var lang = langs.Where(l => l.Contains('S'));
             foreach (var name in lang) {
                 Console.WriteLine(name);
             }
+            Console.WriteLine("");  //改行
 
             //for文
             for (int i = 0; i < langs.Count; i++) {
-                if (langs[i].Contains("S")) {
+                if (langs[i].Contains('S')) {
                     Console.WriteLine(langs[i]);
                 }
             }
+            Console.WriteLine("");  //改行
 
             //While文
             int k = 0;
             while (k < langs.Count) {
-                if (langs[k].Contains("S")) {
+                if (langs[k].Contains('S')) {
                     Console.WriteLine(langs[k]);
                 }
                 k++;
@@ -39,7 +41,28 @@ namespace Exercise01 {
         }
 
         private static void Exercise2(List<string> langs) {
+            //foreach文
+            foreach (var lan in langs) {
+                if (lan.Contains('S')) {
+                    Console.WriteLine(lan);
+                }
+            }
+            Console.WriteLine("");  //改行
 
+            //for文
+            var lang = langs.Where(l => l.Contains('S')).ToList();
+            for(int i = 0; i < lang.Count; i++) {
+                Console.WriteLine(lang[i]);
+            }
+            Console.WriteLine("");  //改行
+
+            //While文
+            var lange = langs.Where(l => l.Contains('S')).ToList();
+            int k = 0;
+            while (k < lange.Count) {
+                    Console.WriteLine(lange[k]);
+                k++;
+            }
         }
 
         private static void Exercise3(List<string> langs) {
