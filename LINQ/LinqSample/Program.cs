6d2b50d2ab3587b5710db1569a1b66dfafc0c665
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-05-27 09:55:41 +0900

    7.1.1完成

---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            //最小値を出力
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Min());
+
+            //合計値を出力
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
+
+            //foreach (var num in numbers) {
+            //    Console.WriteLine(num);
+            //}
+        }
+    }
+}
