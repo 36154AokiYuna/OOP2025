fe1d9a119b4417d10d4e56921f307464b9f1b8f3
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-06-06 15:59:07 +0900

    5.1.3完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -19,12 +19,15 @@ namespace Exercise01 {
         //5.1.2
         //設定されている西暦が２１世紀か判定する
         //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
-        public bool Is21Century(int year) => 2001 <= year && year <= 2100;
-
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
 
         //5.1.3
         public YearMonth AddOneMonth() {
-
+            if(Month == 12) {
+                return new YearMonth(Year + 1, 1); //Monthが12月
+            } else {
+                return new YearMonth(Year, Month + 1); //Monthが12月以外
+            }
         }
 
 
