19fbc10498d706bb3f4dc02d5e3d4a854a70edde
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-06-11 11:00:03 +0900

    8.1.1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var textDict = new Dictionary<Char, int>();
+            foreach (var ch in text.ToUpper()) {
+                if('A' <= ch && ch <= 'Z') {
+                    if (textDict.ContainsKey(ch)) {
+                        textDict[ch] += 1;
+                    } else {
+                        textDict[ch] = 1;
+                    }
+                }
+            }
+            var sort = textDict.OrderBy(c => c.Key).ToList();
+            foreach (var item in sort) {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+        }
+
+        private static void Exercise2(string text) {
+            
+        }
+    }
+}
