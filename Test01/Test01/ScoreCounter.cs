0cff0253e2bf63fd0a22941523d22852ff172ca2
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-06-17 11:27:48 +0900

    設問１_提出

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -4,26 +4,39 @@
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
+            _score = ReadScore(filePath);
         }
 
-        //メソッドの概要： 
+        //メソッドの概要：点数データを読み込み、Studentオブジェクトのリストを返す 
         private static IEnumerable<Student> ReadScore(string filePath) {
             
+            var scores = new List<Student>();
+            
+            string[] lines = File.ReadAllLines(filePath);
 
-
-
-
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                
+                var score = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(score);
+            }
+            return scores;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要：科目別点数を求める 
         public IDictionary<string, int> GetPerStudentScore() {
-            
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
         }
     }
 }
