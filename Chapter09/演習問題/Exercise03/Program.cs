b08a676b109f5ec6446a716ed448e2abc5134492
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-06-27 13:48:08 +0900

    Stopwatchに変更

--- Chapter09/演習問題/Exercise03/Program.cs
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace Exercise03 {
+﻿using System.Diagnostics;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
-            var tw = new TimeWatch();
+            //var tw = new TimeWatch();
+            var tw = new Stopwatch();
             tw.Start();
             // スリープする
-            Thread.Sleep(1000);
-            TimeSpan duration = tw.Stop();
+            Thread.Sleep(2000);
+
+            tw.Stop();
+            TimeSpan duration = tw.Elapsed;
             Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
 
         }
