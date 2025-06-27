d473195d6956435e1d3e5300c8313acb1e6cfb78
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-06-27 10:03:14 +0900

    9.1完成

---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            // 2024/03/09 19:03
+            // string.Formatを使った例
+            var s1 = string.Format("{0:yyyy/MM/dd HH:mm}",dateTime);
+            Console.WriteLine(s1);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            // 2024年03月09日 19時03分09秒
+            // DateTime.ToStringを使った例
+            var s2 = dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒");
+            Console.WriteLine(s2);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var s3 = dateTime.ToString("gg y年 M月 d日", culture);
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            Console.WriteLine($"{s3}({dayOfWeek})");
+        }
+    }
+}
