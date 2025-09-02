0233e6cfa1fcddaa7c6c4de10b9b126f153a38b0
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-09-02 13:24:03 +0900

    Chapter11演習問題完成

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var strings = new[] {
+                "Microsoft Windows",
+                "windows",
+                "Windows Server",
+                "Windows",
+            };
+
+            var regex = new Regex(@"^(W|w)indows$");
+
+            //パターンと完全一致している文字列の個数をカウント
+            var count = strings.Count(s => regex.IsMatch(s));
+            Console.WriteLine($"{count}行と一致");
+
+            //パターンと完全一致している文字列を出力する
+            foreach (var text in strings.Where(s => regex.IsMatch(s))) {
+                Console.WriteLine(text);
+            }
+
+            //別解(このほうが汎用性高い)
+            //var datas = strings.Where(s => regex.IsMatch(s));
+            //foreach (var item in datas) {
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
