f1072107235547c0b5a238b2b048e5cda677fb74
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-09-05 10:34:35 +0900

    WPFアプリのUI構築

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -33,6 +33,35 @@ namespace SampleApplication
             MessageBox.Show("キャンセルされました");
         }
 
+        private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)(seasonComboBox.SelectedItem)).Content;
+        }
+
+
+        private void colorRadioButton_Checked(object sender, RoutedEventArgs e) {
+            //自分の回答
+            //colorText.Text = (string)(redRadioButton).Content;
+            //模範解答
+            colorText.Text = (string)((RadioButton)(sender)).Content;
+            //別解 colorText.Text = "黄";
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        //private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
+        //    colorText.Text = "赤";
+        //}
+
+        //private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+        //    colorText.Text = "青";
+        //}
+
         //private void okButton_Click(object sender, RoutedEventArgs e) {
         //    MessageBox.Show(string.Format("入力された文字は{0}です。", messageTextBox.Text));
         //}
