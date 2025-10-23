5ada57928a28452a36e4ddbbc4a399ac68b08a03
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-10-23 15:59:33 +0900

    更新ができない

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using CustomerApp.Data;
 using Microsoft.Win32;
 using SQLite;
+using System.IO;
+using System.IO.Enumeration;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,12 +20,13 @@ namespace CustomerApp;
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window {
+
+    OpenFileDialog ofd;
+
     private List<Customer> _customers = new List<Customer>();
 
     public MainWindow() {
         InitializeComponent();
-        //↓イメージを作る（変換）
-        //PictureImage.Source = ;
         ReadDatabase();
 
         CustomerListView.ItemsSource = _customers;
@@ -38,25 +41,32 @@ public partial class MainWindow : Window {
 
     //画像ボタン
     private void PictureButton_Click(object sender, RoutedEventArgs e) {
-        OpenFileDialog ofd = new OpenFileDialog();
+        ofd = new OpenFileDialog();
         var ret = ofd.ShowDialog();
         if(ret ?? false) {
-            
+            var bitmap = new BitmapImage(new Uri(ofd.FileName));
+            PictureImage.Source = bitmap;
         }
     }
 
+
+
     //保存ボタン
     private void SaveButton_Click(object sender, RoutedEventArgs e) {
-        var customer = new Customer() {
+
+        Customer customer = new Customer();
+        customer.Picture = File.ReadAllBytes(ofd.FileName);
+
+        var _customer = new Customer() {
             Name = NameTextBox.Text,
             Phone = PhoneTextBox.Text,
             Address = AddressTextBox.Text,
-            //Picture = PictureImage.,
+            Picture = customer.Picture,
         };
 
         using (var connection = new SQLiteConnection(App.databasePath)) {
             connection.CreateTable<Customer>();
-            connection.Insert(customer);
+            connection.Insert(_customer);
         }
 
         ReadDatabase();
@@ -93,7 +103,7 @@ public partial class MainWindow : Window {
                 Name = NameTextBox.Text,
                 Phone = PhoneTextBox.Text,
                 Address = AddressTextBox.Text,
-                //Picture = PictureImage.,
+                Picture = ImageSourceToBytes(PictureImage.Source),
             };
 
             connection.Update(customer);
@@ -112,6 +122,8 @@ public partial class MainWindow : Window {
 
     //リストビューから１レコード選択
     private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+
         var selectedCustomer = CustomerListView.SelectedItem as Customer;
 
         if (selectedCustomer == null) {
@@ -121,6 +133,34 @@ public partial class MainWindow : Window {
         NameTextBox.Text = selectedCustomer.Name;
         PhoneTextBox.Text = selectedCustomer.Phone;
         AddressTextBox.Text = selectedCustomer.Address;
-        //PictureImage. = selectedCustomer.Picture;
+        PictureImage.Source = BytesToImageSource(selectedCustomer.Picture);
     }
+
+    private byte[] ImageSourceToBytes(ImageSource imageSource) {
+        if (imageSource is BitmapSource bitmapSource) {
+            var encoder = new JpegBitmapEncoder(); // JPEGでもOK
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+            using (var stream = new MemoryStream()) {
+                encoder.Save(stream);
+                return stream.ToArray();
+            }
+        }
+        return null;
+    }
+
+    private ImageSource BytesToImageSource(byte[] bytes) {
+        if (bytes == null || bytes.Length == 0) return null;
+
+        var bitmap = new BitmapImage();
+        using (var stream = new MemoryStream(bytes)) {
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.StreamSource = stream;
+            bitmap.EndInit();
+            bitmap.Freeze();
+        }
+        return bitmap;
+    }
+
 }
