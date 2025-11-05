618d8b6d90619d35dee784061e7eade77b9bdc96
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-11-05 10:38:02 +0900

    14.1 コンソールとWPFうつした

--- Chapter14/演習問題/Exercise01_Console/Program.cs
+++ Chapter14/演習問題/Exercise01_Console/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 internal class Program {
-    static void Main(string[] args) {
-        string text = TextReaderSample.ReadText("吾輩は猫である.txt");
+    static async Task Main(string[] args) {
+        string text = await TextReaderSample.ReadText("吾輩は猫である.txt");
         Console.WriteLine(text);
 
         Console.WriteLine("End!");
     }
 
     static class TextReaderSample {
-        public static string ReadText(string filePath) {
+        public static async Task<string> ReadText(string filePath) {
             var sb = new StringBuilder();
 
             // using を使って自動的にファイルを閉じる
             using (var sr = new StreamReader(filePath, Encoding.UTF8)) {
                 while (!sr.EndOfStream) {
-                    string? line = sr.ReadLine();  // 1行読み取り
+                    string? line = await sr.ReadLineAsync();  // 1行読み取り
                     sb.AppendLine(line);
                 }
             }
--- Chapter14/演習問題/Exercise01_Wpf/MainWindow.xaml.cs
+++ Chapter14/演習問題/Exercise01_Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,6 +17,24 @@ namespace Exercise01_Wpf {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        //模範解答
+        private async void Button_Click(object sender, RoutedEventArgs e) {
+            TextArea.Text = await TextReaderSample.ReadTextAsync("吾輩は猫である.txt");
+        }
+
+        static class TextReaderSample {
+            public static async Task<string> ReadTextAsync(string filePath) {
+                var sb = new StringBuilder();
+                var sr = new StreamReader(filePath);
+                while (!sr.EndOfStream) {
+                    var line = await sr.ReadLineAsync();
+                    sb.AppendLine(line);
+                    await Task.Delay(10);  //わざと遅延させる
+                }
+                return sb.ToString();
+            }
         }
     }
 }
