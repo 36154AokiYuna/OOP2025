df160012f76925077708c6980b0a81cf69c33d63
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-11-12 11:55:12 +0900

    ②完成

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -8,15 +8,20 @@ using TextFileProcessor;
 namespace LineCounter {
     internal class LineCounterProcessor : TextProcessor{
         private int _count = 0;
+        private string word = "";
 
-        protected override void Initialize(string fname) => _count = 0;
+        protected override void Initialize(string fname) {
+             _count = 0;
+            Console.Write("カウントしたい単語：");
+            word = Console.ReadLine();
+        }
 
         protected override void Execute(string line) {
-            if (line.Contains("private")) {
+            if (line.Contains(word)) {
                 _count++;
             }
         }
 
-        protected override void Terminate() => Console.WriteLine("{0} 個", _count);
+        protected override void Terminate() => Console.WriteLine($"{word}の個数：{_count}個");
     }
 }
