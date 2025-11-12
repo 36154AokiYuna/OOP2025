1f5cc27a58bc50601a83cae24a65572a2c30071f
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-11-12 11:41:42 +0900

    ①完成

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -11,8 +11,12 @@ namespace LineCounter {
 
         protected override void Initialize(string fname) => _count = 0;
 
-        protected override void Execute(string line) => _count++;
+        protected override void Execute(string line) {
+            if (line.Contains("private")) {
+                _count++;
+            }
+        }
 
-        protected override void Terminate() => Console.WriteLine("{0} 行", _count);
+        protected override void Terminate() => Console.WriteLine("{0} 個", _count);
     }
 }
