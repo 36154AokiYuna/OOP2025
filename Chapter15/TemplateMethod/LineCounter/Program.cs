5d24d8b9dba5535dbf79c98bfcee24c32083a61b
36154AokiYuna <oibc36154@ojs.ac.jp>
2025-11-13 13:33:41 +0900

    パス指定の単語カウント

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -3,7 +3,7 @@
 namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
-            Console.Write("パスを入力：");
+            Console.Write("パスを入力してください：");
             string path = Console.ReadLine();
             TextProcessor.Run<LineCounterProcessor>(path);
         }
